8b782768ff29cef29fb45c8b618e5f62a4248205
Sam Verschueren <sam.verschueren@gmail.com>
2016-11-29 14:12:03 +0100

    fix benefits routing issue

--- src/app/my/my-routing.module.ts
+++ src/app/my/my-routing.module.ts
@@ -2,11 +2,13 @@ import {Routes, RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {MyComponent} from './my.component';
+import {BenefitsModule} from './benefits/benefits.module';
 import {AuthGuard} from '../shared/auth-guard';
 
 const myRoutes: Routes = [{
   path: '', component: MyComponent, canActivate: [AuthGuard], children: [
     {path: '', component: DashboardComponent, pathMatch: 'full'},
+    {path: 'benefits', loadChildren: () => BenefitsModule},
   ]
 }];
 
